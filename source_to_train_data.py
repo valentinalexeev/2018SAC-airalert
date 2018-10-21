dc245f43b30ab3ba6fb5ceff078cf8bb651e74ca
Valentin Alexeev <valentin.alekseev@gmail.com>
2018-10-21 12:15:04 +0300

    Convert ppm to category for trainings

--- source_to_train_data.py
+++ source_to_train_data.py
@@ -1,6 +1,20 @@
 import csv
 import sys
 
+def ppm_to_category(ppm):
+    result = 0
+    if ppm > 300:
+        result = 5
+    elif ppm > 200:
+        result = 4
+    elif ppm > 150:
+        result = 3
+    elif ppm > 100:
+        result = 2
+    elif ppm > 50:
+        result = 1
+    return result
+
 source = csv.reader(sys.stdin)
 
 # use first three lines as source of prediction
@@ -11,7 +25,10 @@ line_3 = source.next()
 result = []
 
 for line in source:
-    result += [[line[1]] + line_3[2:5] + line_2[2:5] + line_1[2:5]]
+    # convert target ppm to category
+
+
+    result += [[ppm_to_category(int(line[1]))] + line_3[2:5] + line_2[2:5] + line_1[2:5]]
     # shift lines
     line_1 = line_2
     line_2 = line_3
